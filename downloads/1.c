f0102b2ba355a8930285e454f9495411918f6485
41223153 <144767366+41223153@users.noreply.github.com>
2023-12-31 07:56:35 +0000

    新增ANSIC

---
+++ downloads/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   : 龔威仁\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : Feb 11, 2005\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 0908-780211\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
