601c59574759346b8854f73ff59c2ddbb15c7434
41223153 <144767366+41223153@users.noreply.github.com>
2024-01-01 17:02:37 +0000

    新增ANSIC程式註解

--- downloads/1.c
+++ downloads/1.c
@@ -3,13 +3,13 @@
 int main()  
 {
     // Print Name
-    printf("Name   : 龔威仁\n"); 
+    printf("Name   : 龔威仁\n"); //使用 printf 函式來印出姓名 "龔威仁
 
     // Print Date of Birth
-    printf("DOB    : Feb 11, 2005\n"); 
+    printf("DOB    : Feb 11, 2005\n"); //使用 printf 函式印出了出生日期 "Feb 11, 2005"
 
     // Print Mobile Number
-    printf("Mobile : 0908-780211\n"); 
+    printf("Mobile : 0908-780211\n"); //使用 printf 函式印出了手機號碼 "0908-780211"
 
     // Indicate successful execution
     return(0); 
--- downloads/4.c
+++ downloads/4.c
@@ -2,12 +2,13 @@
 
 int main() 
 {
-    // Declare and initialize character variables
+    // 宣告並初始化了三個字符變數 char1、char2、char3，分別賦值為 'X'、'M'、'L'，它們分別代表英文字母 X、M     和 L。
     char char1 = 'X';
     char char2 = 'M';
     char char3 = 'L';
 
-    // Print the original and reversed characters
+    // 格式化字串中包含了兩組 %c，每組 %c 用來表示一個字符。
+    // 會先印出 char1、char2、char3 這三個字符，接著印出 char3、char2、char1 這三個字符。這樣做的結果就是     在同一行中先印出 X, M, L，然後接著印出 L, M, X。
     printf("The reverse of %c%c%c is %c%c%c\n",
         char1, char2, char3,
         char3, char2, char1);
